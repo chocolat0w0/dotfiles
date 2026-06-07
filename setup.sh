34a5888f5155da9564d05f004086d62997059600
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2026-06-07 15:56:16 +0900

    Add Ghostty config setup

--- setup.sh
+++ setup.sh
@@ -3,13 +3,18 @@
 echo "start setup"
 
 echo "setup zsh"
-script_dir=$(cd $(dirname $0); pwd)
-ln -fs ${script_dir}/zsh/.zshrc ${HOME}/.zshrc
-ln -nfs ${script_dir}/zsh/.zsh.d ${HOME}/.zsh.d
-source ${HOME}/.zshrc
+script_dir=$(cd "$(dirname "$0")"; pwd)
+ln -fs "${script_dir}/zsh/.zshrc" "${HOME}/.zshrc"
+ln -nfs "${script_dir}/zsh/.zsh.d" "${HOME}/.zsh.d"
+source "${HOME}/.zshrc"
 
 echo "setup karabiner"
-rm -f ~/.config/karabiner/karabiner.json
-ln -s ${script_dir}/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+rm -f "${HOME}/.config/karabiner/karabiner.json"
+ln -s "${script_dir}/karabiner/karabiner.json" "${HOME}/.config/karabiner/karabiner.json"
+
+echo "setup ghostty"
+mkdir -p "${HOME}/.config/ghostty"
+rm -f "${HOME}/.config/ghostty/config.ghostty"
+ln -s "${script_dir}/ghostty/config.ghostty" "${HOME}/.config/ghostty/config.ghostty"
 
 echo "complete setup"
